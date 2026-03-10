eb15fa3fe033eab5db5b1741e8fd4382970962e1
Santiago pabon <santiago.pabon72@gmail.com>
2026-03-09 21:19:16 -0600

    finished classFeed services

--- src/services/classFeed.ts
+++ src/services/classFeed.ts
@@ -6,49 +6,59 @@ TODO: Create the following functions:
 - createComment
 */
 import api from "./api";
-import {User} from "../types";
-import {Comment} from "../types";
-import {Post} from "../types";
-import {getApiErrorMessage} from "./api";
-
+import { User } from "../types";
+import { Comment } from "../types";
+import { Post } from "../types";
+import { getApiErrorMessage } from "./api";
 
 export async function Login(User: User) {
   try {
-    const response = await api.post("/login", {username: User.username,});
+    const response = await api.post("/login", { username: User.username });
     return response.data;
   } catch (error) {
     throw new Error(getApiErrorMessage(error));
   }
 }
-export async function getFeed(token: string, author: string){
-  try{
-    const response = await api.get(`/feed?=${author}`)
+export async function getFeed(token: string, author: string) {
+  try {
+    const response = await api.get(`/feed?=${author}`);
     return response.data;
-  }
-  catch(error){
+  } catch (error) {
     throw new Error(getApiErrorMessage(error));
   }
+}
+export async function createPost(Post: Post) {
+  try {
+    const response = await api.post("/feed", {
+      posts: {
+        id: Post.id,
+        author: Post.author,
+        text: Post.text,
+        createdAt: Post.createdAt,
+        commentCount: Post.commentCount,
+        comments: Post.comments,
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    throw new Error(getApiErrorMessage(error));
+  }
+}
+export async function UpdatePost(Post: Post) {
+  try {
+    const response = await api.put(`/posts/${Post.id}`, { text: Post.text });
+    return response.data;
+  } catch (error) {
+    throw new Error(getApiErrorMessage(error));
+  }
+  
 };
-export async function createPost(Post: Post){
+export async function CreateComment(Comment:Comment){
   try{
-    const response = await api.post("/feed", 
-      {
-        posts: {
-          id: Post.id,
-          author: Post.author,
-          text: Post.text,
-          createdAt: Post.createdAt,
-          commentCount: Post.commentCount,
-          comments: Post.comments
-        }
-      })
-      return response.data;
-  }
-  catch(error){
+    const response = await api.post(`/posts/${Comment.postId}/comments`,{text:Comment.text});
+    return response.data;
+  }catch(error){
     throw new Error(getApiErrorMessage(error));
   }
-
-};
-export async function createComment(Comment: Comment){
-
-};
+}
